ba1e61a6bc3e3d57ac76d6e47e8da5dc2dc335d7
Craig Waite <undead_craig@hotmail.com>
2013-09-19 23:30:14 +1000

    Initial load

---
+++ LocuWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocuWebAPI.WebApiApplication" Language="C#" %>
---
+++ LocuWebAPI/Models/RestaurantModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocuWebAPI.Models
+{
+    public class RestaurantModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class Meta
+{
+    public int limit { get; set; }
+   
+}
+
+    public class Object
+    {
+        public string name { get; set; }
+        public string locality { get; set; }
+        public string street_address { get; set; }
+        public List<object> cuisines { get; set; }
+        public string region { get; set; }
+        public double @long { get; set; }
+        public string phone { get; set; }
+        public string postal_code { get; set; }
+        public List<object> categories { get; set; }
+        public bool has_menu { get; set; }
+        public string country { get; set; }
+        public double lat { get; set; }
+        public string id { get; set; }
+        public string website_url { get; set; }
+        public string resource_uri { get; set; }
+    }
+
+    public class Objects
+    {
+        public string name { get; set; }
+        public string locality { get; set; }
+        public string street_address { get; set; }
+        public List<object> cuisines { get; set; }
+        public string region { get; set; }
+        public double @long { get; set; }
+        public string phone { get; set; }
+        public string postal_code { get; set; }
+        public List<object> categories { get; set; }
+        public bool has_menu { get; set; }
+        public string country { get; set; }
+        public double lat { get; set; }
+        public string id { get; set; }
+        public string website_url { get; set; }
+        public string resource_uri { get; set; }
+    }
+
+}
