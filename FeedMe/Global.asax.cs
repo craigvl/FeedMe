54ae27b50201fd46fb33269f0d163aa8c0f1e8b5
Craig Waite <undead_craig@hotmail.com>
2013-10-08 21:21:25 +1000

    Working Signalr example

--- FeedMe/Global.asax.cs
+++ FeedMe/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace FeedMe
 {
@@ -14,6 +15,7 @@ namespace FeedMe
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapHubs();
             AreaRegistration.RegisterAllAreas();
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
