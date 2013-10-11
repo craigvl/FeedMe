5b5ac739d10c305d9759253c63ab3672b8e830f4
Craig Waite <undead_craig@hotmail.com>
2013-10-11 21:04:38 +1000

    Authentication

--- FeedMe/ChatHub.cs
+++ FeedMe/ChatHub.cs
@@ -3,15 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace FeedMe
 {
+
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, HashSet<string>> _connections =
+            new Dictionary<T, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(T key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+    }
+    
+
     public class ChatHub : Hub
     {
+        private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
+
+        List<string> Customers;
+
         public void Send(string name, string message)
         {
+    
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
+
+            Customers.Add(Context.ConnectionId);
+
+            Clients.Client(Context.ConnectionId).broadcastMessage(name, "Thanks Man");
+
+            var e = Context.ConnectionId;
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Remove(name, Context.ConnectionId);
+
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+
+            return base.OnReconnected();
         }
     }
 }
